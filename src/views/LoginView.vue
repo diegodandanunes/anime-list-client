0878e062623db1d2a32156610ef0b31a0d03c307
diegodandanunes <diegodandanunes@gmail.com>
2024-10-06 09:15:56 -0300

    first commit

---
+++ src/views/LoginView.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="mt-5">
+        <button class=""></button>
+        login
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
